f49aca644253459c7ef63bcbfe02e7baa73c4479
Md. Nazmus Salahin Rocky <rocky.bgta@gmail.com>
2022-03-14 23:37:46 +0600

    in-memory Authentication

--- src/main/java/com/spring/security/and/jpa/enosis/preparation/config/SecurityConfig.java
+++ src/main/java/com/spring/security/and/jpa/enosis/preparation/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.spring.security.and.jpa.enosis.preparation.config;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -26,5 +28,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/contact").permitAll()
                 .and()
                 .formLogin().and().httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("123456").authorities("admin")
+                .and()
+                .withUser("user").password("123456").authorities("read")
+                .and()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 }

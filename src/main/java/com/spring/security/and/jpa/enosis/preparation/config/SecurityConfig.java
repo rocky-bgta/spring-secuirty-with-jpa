dbf5a13bafe0cdbedbb0f6e647d8e48d04740183
Md. Nazmus Salahin Rocky <rocky.bgta@gmail.com>
2022-03-14 22:32:45 +0600

    spring with ant matcher

---
+++ src/main/java/com/spring/security/and/jpa/enosis/preparation/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.spring.security.and.jpa.enosis.preparation.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+//        http.authorizeRequests()
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin().and()
+//                .httpBasic();
+
+        http.authorizeRequests()
+                .antMatchers("/myAccount").authenticated()
+                .antMatchers("/myBalance").authenticated()
+                .antMatchers("/myLoans").authenticated()
+                .antMatchers("/myCards").authenticated()
+                .antMatchers("/notices").permitAll()
+                .antMatchers("/contact").permitAll()
+                .and()
+                .formLogin().and().httpBasic();
+    }
+}
